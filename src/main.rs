4d85031499449c087108a9c22254e8f1ecade436
quephird <danielle_kefford@hotmail.com>
2023-08-12 18:40:21 -0700

    Game now tracks number of guesses and exits at the appropriate times.

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,12 @@
 use std::fs::read_to_string;
 use std::io::stdin;
 use std::iter::zip;
+use std::process::exit;
 
 use rand::seq::SliceRandom;
 use rand::thread_rng;
 use termion::color;
 use termion::color::Color;
-
-fn format_cell<C: Color>(bg_color: C, guess_char: char) -> String {
-    format!(
-        "{}{} {} {}{}",
-        color::Bg(bg_color),
-        color::Fg(color::Black),
-        guess_char,
-        color::Fg(color::Reset),
-        color::Bg(color::Reset),
-    )
-}
 
 #[derive(PartialEq)]
 enum MatchType {
@@ -56,7 +46,18 @@ fn check_guess(guess: &str, actual: &str) -> [MatchType; 5] {
     results
 }
 
-fn format_results(guess: &str, results: [MatchType; 5]) -> String {
+fn format_cell<C: Color>(bg_color: C, guess_char: char) -> String {
+    format!(
+        "{}{} {} {}{}",
+        color::Bg(bg_color),
+        color::Fg(color::Black),
+        guess_char,
+        color::Fg(color::Reset),
+        color::Bg(color::Reset),
+    )
+}
+
+fn format_results(guess: &str, results: &[MatchType; 5]) -> String {
     let mut formatted_result = "".to_string();
     for (guess_char, result) in zip(guess.to_ascii_uppercase().chars(), results) {
         let formatted_cell = match result {
@@ -81,22 +82,32 @@ fn main() {
 
     let mut rng = thread_rng();
     let actual = words.choose(&mut rng).unwrap();
+    let mut guesses = 1;
 
     loop {
+        println!("Enter a guess! ");
         let mut buffer = String::new();
         stdin().read_line(&mut buffer);
         let guess = buffer.trim();
 
         let results = check_guess(guess, actual);
-        let formatted_results = format_results(guess, results);
+        let formatted_results = format_results(guess, &results);
         println!("{}", formatted_results);
+
+        if results.iter().all(|result| *result == MatchType::CorrectPosition) {
+            println!("You win!!!");
+            break;
+        } else if guesses == 6 {
+            println!("You lose :(");
+            println!("The word was: {}", actual);
+            break;
+        } else {
+            guesses += 1;
+        }
     }
     // TODO: Need to validate user input for length
     // TODO: Need to check that word is in dictionary
     // TODO: Need to return to user input if there are validation errors
-    // TODO: Need to end game if guessed word is correct
-    // TODO: Need to track number of guesses
-    // TODO: Need to end game after six guesses
     // TODO: Need to be able to start a new game (like with CTRL-N)
     // TODO: Need to be able to exit cleanly (like with CTRL-D)
     // TODO: Need to print the keyboard!!!
